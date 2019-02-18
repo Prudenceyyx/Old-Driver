53c2eb96c4ee57001111a366d0da6366e354717a
prudence <prudenceyaoyx@gmail.com>
2019-02-18 19:00:17 +0800

    deck.gl

---
+++ public/js/controller.js
@@ -0,0 +1,19 @@
+var GameController = {
+    onstart:;
+    onrunning:;
+    onend:;
+
+}
+
+var TimeController = {
+    onstart:;
+    onrunning:;
+    onend:;
+}
+
+
+var VizController = {
+    onstart:;
+    onrunning:;
+
+}
